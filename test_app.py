1973d5db24a9ec8daa84b7d2e8f1ab1908ba92c2
Ojasvi <ojasvimonga95@gmail.com>
2019-08-30 00:55:00 +0900

    add test to check output length

--- test_app.py
+++ test_app.py
@@ -19,15 +19,24 @@ def test_unknown_path():
     with app.app.test_client() as c:
         response = c.get('/bad_uri')
         assert response.status_code == 404
-    # with app.test_client() as c:
         response = c.get('/images/api/v1/resize')
+        # Because GET method is not supported
         assert response.status_code == 405
 
-#test with bad,incomplete json etc
-#search: python testing: different function for each test?
+# test with bad,incomplete json etc
+# search: python testing: different function for each test?
+
+
 def test_2():
     with app.app.test_client() as c:
         with open('test_input.json') as file:
-            response = c.post('/images/api/v1/resize', json=json.load(file))
-        print(response.status_code)
+            json_data = json.load(file)
+            response = c.post('/images/api/v1/resize', json=json_data)
+        assert response.status_code == 200
+        assert len(response.get_json()['resized_images']) == len(json_data['urls'])
+
+
+def test_3():
+    with app.app.test_client() as c:
+        response = c.post('/images/api/v1/resize', json='{}')
         assert response.status_code == 200
