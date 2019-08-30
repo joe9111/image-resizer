89ac9b58c79ab29b55e327a811ad31324c4ee0d5
Ojasvi Monga <monga_o@worksap.co.jp>
2019-08-30 21:17:28 +0900

    add kafka and docker

--- test_app.py
+++ test_app.py
@@ -2,41 +2,37 @@ import flask
 import os
 import tempfile
 import app
-import pytest
+# import pytest
 import json
 from flask import request
-
-
-# @pytest.fixture
-# def client():
-#     yield app
-
-
-# app = flask.Flask(__name__)
 
 
 def test_unknown_path():
     with app.app.test_client() as c:
         response = c.get('/bad_uri')
         assert response.status_code == 404
+
+
+def test_get_method():
+    with app.app.test_client() as c:
         response = c.get('/images/api/v1/resize')
         # Because GET method is not supported
         assert response.status_code == 405
 
-# test with bad,incomplete json etc
-# search: python testing: different function for each test?
 
-
-def test_2():
+def test_post_method():
     with app.app.test_client() as c:
         with open('test_input.json') as file:
             json_data = json.load(file)
             response = c.post('/images/api/v1/resize', json=json_data)
         assert response.status_code == 200
-        assert len(response.get_json()['resized_images']) == len(json_data['urls'])
+        assert len(response.get_json()['resized_images']) == len(
+            json_data['urls'])
+        get_image_response = c.get(response.get_json()['resized_images'][0])
+        assert get_image_response.status_code == 200
 
 
-def test_3():
+def test_post_method_with_empty_json():
     with app.app.test_client() as c:
         response = c.post('/images/api/v1/resize', json='{}')
         assert response.status_code == 200
