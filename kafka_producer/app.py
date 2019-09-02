6f0a973743511ddb9420341233170bcbfc627818
Ojasvi Monga <monga_o@worksap.co.jp>
2019-09-02 19:41:45 +0900

    add request from consumer microservice

--- kafka_producer/app.py
+++ kafka_producer/app.py
@@ -14,12 +14,13 @@ import json
 import psycopg2
 
 # logging.basicConfig(level=logging.DEBUG)
-# instead of db, store request ids and responses as json/dict
-# consumer will need to make a write to this dict after processing
+
 logger = logging.getLogger('ftpuploader')
 
 # This basically acts as an in-memory database and stores the contents of
 # each request.
+# Instead of db, store request ids and responses as a dict for now
+# The consumer service will write to this dict after processing
 request_map = {}
 app = Flask(__name__)
 
@@ -40,29 +41,28 @@ def store_processed_images():
     """
     processed_images = request.get_json()
     # This can be replaced by an "Insert in DB" step
+    logger.warning(processed_images)
     request_map[processed_images['request_id']] = processed_images['result']
     return 'Success!'
 
 
 @app.route('/images/api/v1/send-request', methods=['POST'])
 def post_resize_request():
-    """Handle post request from the main client API and return a json 
-    response
+    """Handle post request from the main client API and return the request_id using which the client can access the 
+    result later
     """
     logger.warning('listening!')
     try:
-        urls = request.get_json()
+        kafka_message = request.get_json()
     except Exception as e:
         return ('The given json input is incorrect!')
     request_id = str(uuid.uuid4())
-
+    kafka_message['url_root'] = request.url_root
     logger.info('Sending to kafka!')
-    producer.send(topic, key=request_id, value=urls)
-    # producer.send(topic, json.dumps(urls).encode('utf-8'))
+    producer.send(topic, key=request_id, value=kafka_message)
     logger.info('Message sent to kafka!')
     request_map[request_id] = 'Your request is being processed'
     return {'request': request.url_root + 'images/api/v1/get-request/' + request_id}
-    # return process_urls(urls)
 
 
 @app.route('/images/api/v1/get-image/<path:file_name>', methods=['GET'])
@@ -72,7 +72,9 @@ def get_image(file_name):
 
 @app.route('/images/api/v1/get-request/<string:request_id>', methods=['GET'])
 def get_request(request_id):
-    # read from db
+    """Handle request to get the result of an image processing request return a json 
+    response
+    """
     if request_id in request_map:
         return request_map[request_id]
     return 'Request not found, please try again...'
