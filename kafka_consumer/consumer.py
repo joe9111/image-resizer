d3744209c5273b3010c6f9a28fe7811bebabb338
Ojasvi <ojasvimonga95@gmail.com>
2019-09-03 01:22:49 +0900

    use image map to store images

--- kafka_consumer/consumer.py
+++ kafka_consumer/consumer.py
@@ -16,7 +16,7 @@ logger = logging.getLogger('ftpuploader')
 
 
 app = Flask(__name__)
-
+INVALID_URL_MESSAGE = 'The given url is invalid! '
 # connect to Kafka server and pass the topic we want to consume
 consumer = KafkaConsumer(
     'my-topic', group_id='view', bootstrap_servers='kafka:9092',
@@ -32,50 +32,63 @@ def handle_exception_and_get_response(message, exception):
     return message
 
 
-def compress_and_get_output_filename(image_url):
+def compress_and_get_output_file(image_url):
     try:
         response = requests.get(image_url)
     except Exception as e:
-        return handle_exception_and_get_response('Could not open given url! ', e)
+        return handle_exception_and_get_response(INVALID_URL_MESSAGE, e)
 
     size = 100, 100
     try:
         img = Image.open(BytesIO(response.content))
     except Exception as e:
-        return handle_exception_and_get_response('The given url does not point to an image!', e)
+        return handle_exception_and_get_response(INVALID_URL_MESSAGE, e)
     img.thumbnail(size, Image.ANTIALIAS)
     image_directory_name = 'static'
     # succeeds even if directory exists.
     os.makedirs(image_directory_name, exist_ok=True)
     unique_filename = str(uuid.uuid4())
+    byte_io = BytesIO()
+    img.save(byte_io, 'png')
+    byte_io.seek(0)
+    global image_map
+    image_map[unique_filename] = byte_io
+    # return img
     # save and send blobs
+    # create image map id:img_object
+    # create request to images map: 1 to many
     # img.save(
     #     './{image_directory_name}/{unique_filename}.png'.format(**locals()), format='png')
-    # return '{unique_filename}.png'.format(**locals())
+    return '{unique_filename}.png'.format(**locals())
 
 
 def process_urls(urls, url_root):
     output_urls = []
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        for file_name_or_error_string in executor.map(compress_and_get_output_filename, urls):
-            if ".png" in file_name_or_error_string:
+        for file_or_error_string in executor.map(compress_and_get_output_file, urls):
+            if ".png" in file_or_error_string:
                 output_urls.append(
-                    url_root + 'images/api/v1/get-image/' + file_name_or_error_string)
+                    url_root + 'images/api/v1/get-image/' + file_or_error_string)
             else:
-                output_urls.append(file_name_or_error_string)
+                output_urls.append(file_or_error_string)
     output_dictionary = {'result': {'resized_images': output_urls}}
     return output_dictionary
 
 
+# image_map = {}
+
 # Continuously listen to the connection and process messages as recieved
 for msg in consumer:
+    global image_map
+    image_map = {}
     logger.warning('Msg is ' + str(msg))
     processed_images = process_urls(msg.value['urls'], msg.value['url_root'])
     processed_images['request_id'] = msg.key
-    # requests.post(msg.value['url_root']+'images/api/v1/send-images',
-    #               json=processed_images)
+    processed_images['image_map']=image_map
     requests.post('http://host.docker.internal:5000/images/api/v1/send-images',
-                  json=processed_images)
+                  processed_images)
+    # requests.post('http://host.docker.internal:5000/images/api/v1/send-images',
+    #               json=processed_images)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
